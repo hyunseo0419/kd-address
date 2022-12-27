22a563cea502f21140efb3e44a6348d4cd2fb83e
hyun0419 <joshua@mysterico.com>
2022-12-27 13:17:18 +0900

    :sparkles: initial

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  async rewrites() {
+		return [
+			{
+				source: "/:path*",
+				destination: "https://openapi.naver.com/:path*",
+			},
+		];
+}
 }
 
 module.exports = nextConfig
